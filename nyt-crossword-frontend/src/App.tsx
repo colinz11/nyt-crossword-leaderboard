bf8b14910c38cae86e0bbc3a189d5da34342316d
Colin Zhu <33072296+colinz11@users.noreply.github.com>
2025-03-26 21:38:41 -0400

    Update frontend to use MUI components, adjust TypeScript configuration, and enhance package dependencies

--- nyt-crossword-frontend/src/App.tsx
+++ nyt-crossword-frontend/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* App Bar */}
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            NYT Crossword Leaderboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+  
     </div>
   );
 }
